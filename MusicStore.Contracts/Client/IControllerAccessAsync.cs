9dffe159d375bb80d523156bc46f13daced0bd66
Gerhard Gehrer <gerhard.gehrer@indi.systems>
2019-12-11 19:27:37 +0100

    Add project files.

---
+++ MusicStore.Contracts/Client/IControllerAccessAsync.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Contracts.Client
+{
+    /// <summary>
+    /// This interface defines the basic properties and basic operations for accessing the controller.
+    /// </summary>
+    /// <typeparam name="T">Type, which the basic operations relate.</typeparam>
+    public partial interface IControllerAccess<T> : IDisposable
+        where T : Contracts.IIdentifiable
+    {
+        #region Async-Methods
+        Task<int> CountAsync();
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync();
+        Task<T> InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task SaveChangesAsync();
+        #endregion Async-Methods
+    }
+}
